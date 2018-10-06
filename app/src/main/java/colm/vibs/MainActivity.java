d05e6e7b768cac3670c5b5089e1ef15df9bee89b
Reneboy Torres <nyeboy619@gmail.com>
2018-10-06 11:16:08 +0800

    VibsEffect still not Working

--- app/src/main/java/colm/vibs/MainActivity.java
+++ app/src/main/java/colm/vibs/MainActivity.java
@@ -1,7 +1,9 @@
 package colm.vibs;
 
 import android.app.*;
+import android.content.*;
 import android.os.*;
+import android.widget.*;
 
 public class MainActivity extends Activity 
 {
@@ -10,5 +12,20 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+				TextView text = findViewById(R.id.t);
+					
+			
+				
+				Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+				if (vibrator.hasVibrator()||vibrator.hasAmplitudeControl()) {
+						while(true){
+								VibrationEffect e = VibrationEffect.createOneShot(1000,VibrationEffect.DEFAULT_AMPLITUDE);
+						vibrator.vibrate(e); // for 500 ms
+						}
+				}else{
+						text.setText("No Amp");
+				}
     }
-}
+		}
+		
+		 
