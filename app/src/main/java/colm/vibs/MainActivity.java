4484f68a133658ecf02e4a864049f84e4de07cb8
Reneboy Torres <nyeboy619@gmail.com>
2018-10-08 12:31:52 +0800

    finish

--- app/src/main/java/colm/vibs/MainActivity.java
+++ app/src/main/java/colm/vibs/MainActivity.java
@@ -3,29 +3,86 @@ package colm.vibs;
 import android.app.*;
 import android.content.*;
 import android.os.*;
+import android.view.*;
 import android.widget.*;
 
 public class MainActivity extends Activity 
 {
+		Vibrator vibrator;
+		boolean rock;
+		
+		
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+				
+			
 				TextView text = findViewById(R.id.t);
 					
-			
+		
 				
-				Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-				if (vibrator.hasVibrator()||vibrator.hasAmplitudeControl()) {
-						while(true){
-								VibrationEffect e = VibrationEffect.createOneShot(1000,VibrationEffect.DEFAULT_AMPLITUDE);
-						vibrator.vibrate(e); // for 500 ms
-						}
+				vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+				rock= false;
+				
+				
+		}
+		
+		public void tL(View v){
+				if(rock){
+						rock=false;
 				}else{
-						text.setText("No Amp");
+						rock=true;
 				}
-    }
+				if(rock){
+						vibrator.vibrate(1000000);
+				}else{
+						vibrator.cancel();
+						rock=false;
+				}
+				
+				
+				
+		}
+
+		@Override
+		protected void onPause()
+		{
+				// TODO: Implement this method
+				super.onPause();
+				onStop();
+		}
+		
+		
+
+		@Override
+		protected void onStop()
+		{
+				// TODO: Implement this method
+				super.onStop();
+				onDestroy();
+		}
+		
+		
+
+		@Override
+		protected void onDestroy()
+		{
+				// TODO: Implement this method
+				super.onDestroy();
+				
+				
+				
+		}
+		
+		
+		
+		
+		
+	
+		
 		}
 		
 		 
